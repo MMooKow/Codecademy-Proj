918e40c8cf880563c7c22d4a0eb392655f6f2c54
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-09-03 21:46:49 -0500

    Worked through early JS proj

---
+++ KelvinWeather.js
@@ -0,0 +1,13 @@
+﻿//Initial temperature set to a constant of 293 degrees Kelvin.
+const kelvin = 293;
+//Converting degrees Kelvin to degrees Celsius 
+const celsius = kelvin - 273;
+//Converting from Celcius to Fahrenheit.
+let fahrenheit = celsius*(9/5)+32;
+//Rounding answer down
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+//Convert to the Newton scale
+newton = celsius*(33/100);
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`)
