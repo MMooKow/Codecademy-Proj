ecba36fd61b77b32d6914aa87b53f26ab6738928
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-09-05 09:28:17 -0500

    Created Magic 8 Ball pro project two ways
    Use switch statement and if, else if, else statement to create different versions.

---
+++ EightBallElseIf.js
@@ -0,0 +1,27 @@
+﻿let userName = 'Dylan';
+if (userName){
+  console.log('Hello, ' + userName);
+}else{
+  console.log('Hello!');
+}
+let userQuestion = 'Eat more chikin?';
+console.log('Your question is, ' + userQuestion);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+if (randomNumber === 0){
+    eightBall = console.log('It is certain');
+}else if (randomNumber === 1){
+    eightBall = console.log('It is decidedly so');
+}else if (randomNumber === 2){
+    eightBall = console.log('Reply hazy try again');
+}else if (randomNumber === 3){
+    eightBall = console.log('Cannot predict now');
+}else if (randomNumber === 4){
+  eightBall = console.log('Do not count on it');
+}else if (randomNumber === 5){
+  eightBall = console.log('My sources say no');
+}else if (randomNumber === 6){
+  eightBall = console.log('Outlook not so good');
+}else{
+  eightBall = console.log('Signs point to yes');
+}
---
+++ EightBallSwitch.js
@@ -0,0 +1,36 @@
+﻿let userName = 'Dylan';
+if (userName){
+  console.log('Hello, ' + userName);
+}else{
+  console.log('Hello!');
+}
+let userQuestion = 'Eat more chikin?';
+console.log('Your question is, ' + userQuestion);
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch (randomNumber){
+  case 0:
+   eightBall = console.log('It is certain');
+    break;
+  case 1:
+    eightBall = console.log('It is decidedly so');
+    break;
+  case 2:
+    eightBall = console.log('Reply hazy try again');
+    break;
+  case 3:
+    eightBall = console.log('Cannot predict now');
+    break;
+  case 4:
+    eightBall = console.log('Do not count on it');
+    break;
+  case 5:
+    eightBall = console.log('My sources say no');
+    break;
+  case 6:
+    eightBall = console.log('Outlook not so good');
+    break;
+  case 7:
+    eightBall = console.log('Signs point to yes');
+    break;
+}
