325c83a019b6ba34b8d026c5d1e5092662ad82a1
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-09-06 17:37:36 -0500

    Rock, Paper, Scissors pro proj
    Began working on previous night. Got stuck on second function. Picked up in AM and figured out issue. Working on function to compare choices.

---
+++ RockPapScis.js
@@ -0,0 +1,39 @@
+﻿//Get user decision on their move
+const getUserChoice = userInput =>{
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){ 
+   console.log(userInput);                      }else{
+  console.log('Error\: Invalid Input'); }
+};
+//Get computer move through RNG
+function getComputerChoice(){
+ let compInput = ""
+ let randomNum = Math.floor(Math.random() *3);
+  switch (randomNum){
+    case 0:
+      compInput = "rock";
+      console.log(compInput);
+      break;
+    case 1:
+      compInput = "paper";
+      console.log(compInput);
+      break;
+    case 2:
+      compInput = "scissors";
+      console.log(compInput);
+      break;
+  }
+}
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice){
+      console.log('Tie!');
+      }else if (userChoice = 'rock' && computerChoice === 'paper') {
+      console.log('Player wins!');
+   }else if (userChoice === 'paper' && computerChoice === 'rock') {
+      console.log('Player wins!');
+   }else if (userChoice === 'scissors' && computerChoice === 'paper'){
+      console.log('Player wins!');
+   }else{
+     console.log('Computer wins!');
+   }
+}
