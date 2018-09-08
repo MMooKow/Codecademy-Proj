0955f7066dfaf706f81a895563fa111a8eca8884
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-09-08 13:08:56 -0500

    Began Sleep Debt Calculator pro project

---
+++ SleepCalculator.js
@@ -0,0 +1,23 @@
+﻿//Function for any given nights rest
+const getSleepHours = day =>{
+  switch (day){
+    case 'Monday':
+      return 8;
+    case 'Tuesday':
+      return 8;
+    case 'Wednesday':
+      return 8;
+    case 'Thursday':
+      return 8;
+    case 'Friday':
+      return 8;
+    case 'Saturday':
+      return 8;
+    case 'Sunday':
+      return 8;
+  }
+}
+//Actual hours slept
+const getActualSleepHours = () =>{
+  
+}
