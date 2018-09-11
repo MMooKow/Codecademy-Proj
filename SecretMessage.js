6df8788d73a4e2b3b3a26794a1c7d7cf8fad0fa5
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-09-11 14:16:13 -0500

    Secret Message pro proj
    Completed Secret Message project. Used array methods to decode message

---
+++ SecretMessage.js
@@ -0,0 +1,13 @@
+﻿let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+secretMessage.push('to', 'Program');
+secretMessage[7] = 'right';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6, 1, 'know');
+secretMessage.splice(7, 1, 'know');
+secretMessage.splice(8, 1, 'know');
+secretMessage.splice(9, 1, 'know');
+secretMessage.splice(10, 1, 'know');
+
+console.log(secretMessage.join(' '));
