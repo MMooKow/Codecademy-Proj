37861e32bfc71aec6f80720a63a063da395e9694
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-09-12 16:12:33 -0500

    Whale Speak pro proj
    Take only vowels from a string, capitalize, and return to console.

---
+++ WhaleSpeak.js
@@ -0,0 +1,14 @@
+﻿const input = "turpentine and turtles";
+const vowels = ['a','e','i','o','u'];
+let resultArray = [];
+for (let i = 0; i < input.length; i++){
+  for (let j = 0; j < vowels.length; j++){
+       if (vowels[j] === input[i]){
+           resultArray += input[i];
+         if (vowels[j] === 'e' || vowels[j] === 'u'){
+           resultArray += input[i];
+         }
+       }
+}
+}
+console.log(resultArray.toUpperCase());
