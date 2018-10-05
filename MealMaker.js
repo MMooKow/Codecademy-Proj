1f93eeb61813e56c49528b737877933a94c37033
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-10-05 13:08:28 -0500

    Began Meal Maker proj

---
+++ MealMaker.js
@@ -0,0 +1,40 @@
+﻿const menu = {
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: []
+  },
+  get appetizers() {
+  
+},
+    get mains() {
+  
+},
+    get desserts() {
+  
+},
+    set appetizers(appetizerIn) {
+  
+},
+    set mains(mainsIn) {
+  
+},
+    set desserts(dessertIn) {
+    
+  },
+  get courses() {
+    return {
+      appetizers: this._courses.appetizers,
+      mains: this._courses.mains,
+      desserts: this._courses.desserts
+    }
+  },
+addDishToCourse (courseName, dishName, dishPrice) {
+  const dish = {
+  name: dishName,
+  price: dishPrice
+  //this._courses[courseName].push(dish);
+}
+}
+}
+};
