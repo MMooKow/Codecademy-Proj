83e466c753efc2dccaf69c6f3e730620924ba79d
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-10-07 21:07:21 -0500

    Started Team Stats pro proj
    Team Stats takes game and player stats and provides them to the user plainly or allows the user to use them in various ways.

---
+++ TeamStats.js
@@ -0,0 +1,23 @@
+﻿const team = {
+  _players: [{firstName: 'Bob',
+              lastName: 'Franklin',
+              age: 56},
+            {firstName: 'Ben',
+             lastName: 'Grim',
+             age: 30},
+             {firstName: 'John',
+              lastName: 'Bobson',
+              age: 25}],
+  _games: [{opponent: “Bulldogs”,
+            teamPoints: 42, 
+            opponentPoints: 27},
+           {opponent: “Peppercorns”,
+            teamPoints: 29,
+            opponentPoints: 38},
+           {opponent: “Seahorses”,
+            teamPoints: 49,
+            opponentPoints: 36}],
+  get players () {
+    
+  }
+};
