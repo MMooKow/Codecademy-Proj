be67d8d0afe2fceb1065e2363bc719b8af5d7b0f
Dylan S Restivo <40441070+MMooKow@users.noreply.github.com>
2018-10-12 13:32:10 -0500

    Finished adding classes
    Finished adding classes. Working on details and testing.

--- BuildALibrary.js
+++ BuildALibrary.js
@@ -1,11 +1,7 @@
 ﻿class Media{
-  constructor(title) {
-    this._title = title;
+  constructor() {
     this._isCheckedOut = false;
     this._ratings = [];
-  }
-  get title() {
-    return this._title;
   }
     get isCheckedOut() {
     return this._isCheckedOut;
@@ -14,14 +10,14 @@
     return this._ratings;
   }
   getAverageRating() {
-    this.ratings.reduce(add, 0);
-
-function add(a, b) {
-    return a + b;
+    let sum = 0;
+    for (let i = 0; i <= this.ratings.length -1; i++){
+      sum += this.ratings[i];
+    }
+    sum /= this.ratings.length;
+    return sum
 }
-    //let round = Math.round(this.ratings * 10) / 10;
-  }
-  
+
   toggleCheckOutStatus(status) {
     if (status = true){
       this._isCheckedOut = true;
@@ -43,11 +39,59 @@ addRating(score) {
   }
 }
 }
-const book = new Media('Franklin');
-book.addRating(4.567);
-book.addRating(5);
-book.addRating(2);
-book.addRating(3.2456);
-book.addRating(2.24);
-console.log(book.ratings);
-console.log(book.getAverageRating());
+class Book extends Media{
+  constructor(title, author, pages){
+    this._title = title;
+    this._author = author;
+    this._pages = pages;
+  }
+  get title() {
+    return this._title;
+  }
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+}
+  class Movie extends Media{
+    constructor(title, director, runTime){
+      this._title = title;
+      this._director = director;
+      this._runTime = runTime;
+    }
+    get title() {
+      return this._title;
+    }
+    get director() {
+      return this._director;
+    }
+    get runTime() {
+      return this._runTime;
+    }
+  }
+class Cd extends Media{
+  constructor(title, artist, songs){
+    this._title = title;
+    this._artist = artist;
+    this.songs = songs;
+  }
+  get title() {
+    return this._title;
+  }
+  get artist() {
+    return this._artist;
+  }
+  get songs() {
+    return this._songs;
+  }
+}
+const book1 = new Book('Franklin', 'BillyBob', 2987);
+book1.addRating(4.567);
+book1.addRating(5);
+book1.addRating(2);
+book1.addRating(3.2456);
+book1.addRating(2.24);
+console.log(book1.ratings);
+console.log(book1.getAverageRating());
